943bf4cf11c54e773f3e9082e57c9318a54c16af
RodrigoJManzo <rodrigoj.manzo@gmail.com>
2022-05-30 03:10:29 -0300

    agrego nav bar sencilla

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+export default function NavBar(){
+    return(
+        <header className="realHeader">
+            <nav>
+                <h1> <strong> <a href="">SUPERTEC</a></strong></h1>
+                <ul>
+                    <li> <a href="">PRODUCTOS</a> </li>
+                    <li> <a href="">TIENDA</a></li>
+                    <li> <a href="">NOSOTROS</a></li>
+                </ul>
+            </nav>
+        </header>
+        
+    )
+}
