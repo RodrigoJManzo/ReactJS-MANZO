15c98ca404a695b53abdbff1813a18959930e37d
RodrigoJManzo <rodrigoj.manzo@gmail.com>
2022-06-15 00:00:42 -0300

    arreglo las imagenes de los productos

--- src/components/items/items.js
+++ src/components/items/items.js
@@ -4,35 +4,35 @@ let productos = [
         nombre: "Tarjeta de Video",
         modelo: "RTX3060",
         precio: "US$800",
-        picureURL:"https://images.app.goo.gl/ECueg48BuxFPtq9n9"
+        picureURL:"https://images.nvidia.com/aem-dam/Solutions/geforce/ampere/rtx-3060-ti/geforce-rtx-3060-ti-product-gallery-full-screen-3840-2-bl.jpg"
     },
     {
         id:"2",
         nombre: "Monitor",
         modelo: "MSI Optix 27",
         precio: "US$500",
-        picureURL:"https://images.app.goo.gl/8gRJ1ywAmybdb3SL7"
+        picureURL:"https://asset.msi.com/resize/image/global/product/product_16073221819eda271d0012e9dafad761ec039b1fdd.png62405b38c58fe0f07fcef2367d8a9ba1/600.png"
     },
     {
         id:"3",
         nombre: "MotherBoard",
         modelo: "MSI Plus",
         precio: "US$400",
-        picureURL:"https://images.app.goo.gl/t2mfA6VokrnQmiBx5"
+        picureURL:"https://asset.msi.com/resize/image/global/product/product_3_20190716132640_5d2d601054421.png62405b38c58fe0f07fcef2367d8a9ba1/600.png"
     },
     {
         id:"4",
         nombre: "Combo Teclado y Mouse Razer",
         modelo: "Rzer Viper y Razer Chroma",
         precio: "US$120",
-        picureURL:"https://images.app.goo.gl/bTy7LWJk9mENSNn79"
+        picureURL:"https://febo.com.uy/wp-content/uploads/2019/12/674715-MLU40076653944_122019-O-277x217.jpg"
     },
     {
         id:"5",
         nombre: "Procesador Ryzen",
         modelo: "Ryzen 9",
         precio: "US$700",
-        picureURL: "https://images.app.goo.gl/XU1qAsDzG69Nw2hj7"
+        picureURL: "https://www.amd.com/system/files/styles/600px/private/2019-11/238593-ryzen-9-pib-left-facing-1260x709_0.png?itok=EKzdDkTN"
     }
 ]
 
