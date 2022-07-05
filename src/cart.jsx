1f4d8687860a7a7b1cc212a853eae90237a39a94
RodrigoJManzo <rodrigoj.manzo@gmail.com>
2022-07-05 00:25:02 -0300

    agrego funciones para guardar las ordenes en firestore, y para actualizar y modificar stock

--- src/cart.jsx
+++ src/cart.jsx
@@ -1,11 +1,46 @@
 import {useCartContext } from "./contexts/cartContext"
 import { Link } from "react-router-dom";
+import { addDoc, collection, doc, documentId, getDocs, getFirestore, Query, query, updateDoc, where, writeBatch } from "firebase/firestore";
 
 
 
 
 const Carrito = () => {
     const{cart, IconCart, PriceTotal, DelProducto, VaciarCarrito} = useCartContext();
+
+    async function generarOrden(e){
+        e.preventDefault()
+        let order = {};
+
+        order.buyer = {name:'', email: '' , phone: '' }
+        order.total = PriceTotal()
+
+        order.itmes = cart.map(cartItem => {
+            const id = cartItem.id
+            const nombre = cartItem.nombre
+            const precio = cartItem.precio*cartItem.cantidad
+            return{id, nombre, precio}
+        })
+
+        const db = getFirestore()
+        const queryCollectionStock = collection(db, 'productos')
+
+        const queryActualizarStock = await query(
+            queryCollectionStock, 
+            where(documentId(), 'in', cart.map(it =>it.id))
+        )
+
+        const batch = writeBatch(db)
+
+        await getDocs(queryActualizarStock)
+        .then(resp => resp.docs.forEach(res => batch.update(res.ref,{stock: res.data().stock - cart.find(item => item.id === res.id).cantidad})))
+        .finally(()=> console.log(VaciarCarrito()))
+
+        batch.commit()
+    }
+
+
+
     let agregados = IconCart()
         if (cart.length < 1) {
             return(
@@ -35,13 +70,16 @@ const Carrito = () => {
                                 <spacer>--</spacer>
                                 Cantidad : {item.cantidad}
                                 <spacer>--</spacer>
-                                <button onClick={DelProducto}>Eliminar Producto</button>
+                                <button className="btn btn-outline-danger" onClick={DelProducto}>Eliminar Producto</button>
                                 </li> )}
                         </ul>
                         <div>
-                            <button onClick={VaciarCarrito} >VACIAR CARRITO</button>
+                            <button className="btn btn-outline-danger" onClick={VaciarCarrito} >VACIAR CARRITO</button>
                              <p>El precio de los Productos a comprar es US$ {PriceTotal()}</p>
                             {agregados <= 1 ? <p></p> : <p>La cantidad de objetos en el Carrito es {agregados}</p>}
+                        </div>
+                        <div>
+                            <button className="btn btn-outline-primary" onClick={generarOrden}>Terminar Compra</button>
                         </div>
                     </div>
 
